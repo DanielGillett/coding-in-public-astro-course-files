8f0f6ff505c1a4fd4ff2091eb230463bb7eabd7b
Daniel Gillett <daniel@danielgillett.com>
2024-05-30 21:54:53 +0100

    Lesson-64 - Integrations and Addons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: "https://stargazers.club",
+  integrations: [icon(), sitemap()],
   output: "hybrid",
-  adapter: netlify()
+  adapter: netlify(),
+  prefetch: {
+    prefetchAll: true,
+  }
 });

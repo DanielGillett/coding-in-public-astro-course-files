0470434abe5a0ceaf8742722a049ffbbc6c55262
Daniel Gillett <daniel@danielgillett.com>
2024-06-01 11:16:24 +0100

    Lesson 66 - Image Service Overview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,4 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import icon from "astro-icon";
-
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://stargazers.club",
-  integrations: [icon(), sitemap()],
-  output: "hybrid",
-  adapter: netlify(),
-  prefetch: {
-    prefetchAll: true,
-  }
-});
+export default defineConfig({});

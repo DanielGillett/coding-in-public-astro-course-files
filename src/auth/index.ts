7a448421d48e809a728ddba3f3d8256a4f624fb6
Daniel Gillett <daniel@danielgillett.com>
2024-08-07 16:04:47 +0100

    Lesson 112 - Lucia #5: Logout User

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,7 +2,11 @@ import { Lucia } from "lucia";
 import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
 import { db, Session, User } from "astro:db";
 
-const adapter = new DrizzleSQLiteAdapter(db as any, Session, User);
+const adapter = new DrizzleSQLiteAdapter(
+  db as any,
+  Session as any,
+  User as any
+);
 
 export const lucia = new Lucia(adapter, {
   sessionCookie: {

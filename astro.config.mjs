370798cdb40217417dcd20d561383962a4376b5f
Daniel Gillett <daniel@danielgillett.com>
2024-08-08 17:14:35 +0100

    Lesson 126 - User language preference

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "en",
-        locales: ["es", "en"],
-        routing: {
-            prefixDefaultLocale: true
-        },
-        fallback: {
-            es: "en"
-        }
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false
+    },
+    fallback: {
+      es: "en"
     }
+  },
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });

707bf7cc295e17949284f39a2d5b39934b06ae7f
Daniel Gillett <daniel@danielgillett.com>
2024-08-09 10:59:16 +0100

    Lesson 130 - Astro config Options

--- src/i18n/defaultLangOptions.ts
+++
@@ -1,7 +0,0 @@
-export const Languages = [
-  { name: "English", value: "en" },
-  { name: "Español", value: "es" },
-] as const; //read only
-
-export const defaultLanguage = "en";
-export type Lang = (typeof Languages)[number]["value"];
